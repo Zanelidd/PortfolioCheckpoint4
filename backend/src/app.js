b5dc8689404ccc57dc048321bb5491a3bc7a4e0d
Damien Arnaud <113262967+Zanelidd@users.noreply.github.com>
2023-08-14 10:55:44 +0200

    modif pour test deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+    origin: process.env.FRONTEND_URL ,
     optionsSuccessStatus: 200,
     credentials: true,
   })
